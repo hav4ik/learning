cf3de3ff030c4bb5461b2fdc3c9e878528899ce7
hav4ik <hav4ik@users.noreply.github.com>
2019-07-26 04:29:49 +0300

    deploy

--- texlive-ci/texlive_build.py
+++ texlive-ci/texlive_build.py
@@ -7,12 +7,13 @@ import subprocess
 def main():
     parser = ArgumentParser()
     parser.add_argument('files')
+    parser.add_argument('--deploy')
 
     args = parser.parse_args()
-    walk(args.files)
+    walk(args.files, args.deploy)
 
 
-def build(root_dir, file_name, prog):
+def build(root_dir, file_name, prog, deploy_dir):
     file_path = os.path.join(root_dir, file_name)
     if not prog.match(file_path):
         return
@@ -26,12 +27,19 @@ def build(root_dir, file_name, prog):
     except subprocess.CalledProcessError:
         print("Failed to build %s." % (file_path,))
 
+    if deploy_dir is not None:
+        if not os.path.isdir(deploy_dir):
+            os.makedirs(deploy_dir)
+        pdf_path = os.path.splitext(file_path)[0] + '.pdf'
+        deploy_path = os.path.join(deploy_dir, pdf_path)
+        subprocess.call(["mv", pdf_path, deploy_path])
 
-def walk(pattern):
+
+def walk(pattern, deploy_dir):
     prog = re.compile(pattern)
     for root, dirs, files in os.walk("."):
         for file_name in files:
-            build(root, file_name, prog)
+            build(root, file_name, prog, deploy_dir)
 
 
 if __name__ == '__main__':
